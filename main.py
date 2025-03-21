1e3aab7f2c6a70956690b4cf25c4584e2665b7d1
rmartinez19luc <rmartinez19@luc.edu>
2025-03-21 04:29:11 +0000

    Bugs with options, if user choose something outside of options, it wouldn't process and if they did but then choose a correct option, the program wouldn't recongize this as an updated choice

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     name = input("Welcome player to Challenging Days! What do you wish to be called? ")
-    print("You will traverse your days, interacting with different people in your life. However, you seem to have woken up on the"
-    "wrong side of the bed. Good luck going through, your challenging days! ;)")
+    print("\nYou will traverse your days, interacting with different people in your life. However, you seem to have woken up on the"
+    " wrong side of the bed. Good luck going through, your challenging days! ;)")
 
     reputation = 100
 
@@ -17,30 +17,33 @@ def main():
     scenerio_ten(reputation, name)
 
 def scenerio_one(reputation, name):
-    print("You wake up and tap your phone to check the time. Your phone is dead."
+    print("\n\n\033[1mDAY 1: MORNING DISASTER\033[0m")
+    print("\nYou wake up and tap your phone to check the time. Your phone is dead."
     " You get up to check the time on your laptop and realize you're super late!"
     " On the way out, you tell your mom that your charger must be broken because your "
     "phone didn't charge.\n"
     "Your mom admits she unplugged your phone at night because she needed a charger "
     "and forgot to plug your phone back in.\n"
-    "What do you respond with, " + str(name) + "?\n"
+    "\nWhat do you respond with, " + str(name) + "?\n"
     "a) What the hell is your problem Mom?\n"
     "b) I'm late and its all your fault!\n"
     "c) Of course you would do something like that. *scoffs at her*")
 
-    option = input("Choose an option by writing a, b, or c: ").strip().lower()
-
-    if option == "a":
-        good_option(reputation)
-
-    if option == "b":
-        decent_option(reputation)
-    
-    if option == "c":
-        bad_option(reputation)
+    option = input("\nChoose an option by writing a, b, or c: ").strip().lower()
 
     while option not in ["a", "b", "c"]:
-        option = input("Invalid choice. Choose between options a, b, or c: ").strip().lower()
+        option = input("\nInvalid choice. Choose between options a, b, or c: ").strip().lower()
+
+    if option == "a":
+        reputation = good_option(reputation)
+
+    elif option == "b":
+        reputation = decent_option(reputation)
+    
+    else:
+        reputation = bad_option(reputation)
+
+    return reputation
 
 def scenerio_two(reputation, name):
     print("Premise, premise, premise")
